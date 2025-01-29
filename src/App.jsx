0e1cf3e2a2e82057bfe62f103cd226e956394885
aashish0770 <60351454+aashish0770@users.noreply.github.com>
2025-01-29 14:02:37 +0545

    side bar

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,8 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div className="flex h-screen overflow-hidden">
-      
       <div className="fixed inset-0 z-0">
-        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 vai-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900" />
         <div className="absolute inset-0 backdrop-blur-sm" />
       </div>
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import Header from "../components/Header";
+import { motion } from "framer-motion";
+import StatCart from "../components/StatCart";
+import { BarChart, ShoppingBag, Users, Zap } from "lucide-react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Overview" />
+      <main className="max-w-7xl mx-auto px-4  py-6 lg:px-8 xl:px-12">
+        {/* STATS */}
+        <motion.div
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCart
+            name="Total Sales"
+            icon={Zap}
+            value="$1234"
+            color="#6366f1"
+          />
+          <StatCart
+            name="New Users"
+            icon={Users}
+            value="1234"
+            color="#*8B5CF6"
+          />
+          <StatCart
+            name="Total Products"
+            icon={ShoppingBag}
+            value="1234"
+            color="#EC4899"
+          />
+          <StatCart
+            name="Conversion Rate"
+            icon={BarChart}
+            value="12.34%"
+            color="#10B981"
+          />
+        </motion.div>
+
+        {/* Charts */}
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4"></div>
+      </main>
+    </div>
+  );
 }
 
 export default HomePage;
