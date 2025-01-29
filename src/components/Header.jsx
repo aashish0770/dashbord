bddb246f18a10a5baee24a7b12b4c92bd64bf43c
aashish0770 <60351454+aashish0770@users.noreply.github.com>
2025-01-29 14:03:13 +0545

    header and cart

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = ({ title }) => {
+  return (
+    <header className="bg-gray-800 bg-opacity-50 p-2 backdrop-blur-md shadow-lg border-b border-gray-700">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="text-2xl font-bold text-white">{title}</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
